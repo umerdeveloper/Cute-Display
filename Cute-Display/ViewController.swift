c7608fa6026cc2043948c2bb028fdd98e7b44dfa
umerdeveloper <umar_s66@outlook.com>
2020-04-17 23:09:27 +0500

    Initial Commit

---
+++ Cute-Display/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cute-Display
+//
+//  Created by Umer Khan on 17/04/2020.
+//  Copyright © 2020 Umer Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
