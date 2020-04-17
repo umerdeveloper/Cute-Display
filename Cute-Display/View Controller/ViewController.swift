b3396f35b712f2a4f877b016812c8596f3dfbfe4
umerdeveloper <umar_s66@outlook.com>
2020-04-17 23:17:08 +0500

    Configure View

--- Cute-Display/View Controller/ViewController.swift
+++ Cute-Display/View Controller/ViewController.swift
@@ -12,9 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureView()
+        
     }
-
-
+    
+    
+    fileprivate func configureView() {
+        self.view.backgroundColor = .white
+    }
 }
 
