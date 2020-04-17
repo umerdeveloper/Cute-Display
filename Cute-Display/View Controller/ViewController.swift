ecd3b164186b5b1aedd5a5da9271e21cea9d3dce
umerdeveloper <umar_s66@outlook.com>
2020-04-17 23:58:22 +0500

    collectionView Implemented

--- Cute-Display/View Controller/ViewController.swift
+++ Cute-Display/View Controller/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let label = UILabel()
+    
+    fileprivate let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(CollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return cv
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
+        configureCollectionView()
         
     }
     
@@ -20,5 +33,49 @@ class ViewController: UIViewController {
     fileprivate func configureView() {
         self.view.backgroundColor = .white
     }
+    
+    
+    fileprivate func configureLabel() {
+        view.addSubview(label)
+        label.text = "Cute-Display"
+        label.textColor = .orange
+        label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+    }
+    
+    
+    fileprivate func configureCollectionView() {
+        self.view.addSubview(collectionView)
+        collectionView.backgroundColor = .white
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
 }
 
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.width / 2)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        return cell
+    }
+    
+    
+}
---
+++ Cute-Display/View/CollectionViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  CollectionViewCell.swift
+//  Cute-Display
+//
+//  Created by Umer Khan on 17/04/2020.
+//  Copyright © 2020 Umer Khan. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    fileprivate let wallpaper: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFit
+        iv.image = #imageLiteral(resourceName: "profile")
+        iv.layer.cornerRadius = 12
+        return iv
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(wallpaper)
+        wallpaper.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        wallpaper.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        wallpaper.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        wallpaper.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
