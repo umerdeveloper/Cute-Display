0d1fbe79f4f0dd99f9211926aaa267b55c7e2e5b
umerdeveloper <umar_s66@outlook.com>
2020-04-17 23:12:44 +0500

    first commit

---
+++ Cute-Display/Supporting Files/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  Cute-Display
+//
+//  Created by Umer Khan on 17/04/2020.
+//  Copyright © 2020 Umer Khan. All rights reserved.
+//
+
+import UIKit
+@available(iOS 13.0, *)
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
